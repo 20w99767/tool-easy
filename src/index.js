8c08b5e3a8fcdd95e50e34d2390348131c8917b6
20w99767 <wrlsqzmlq160@hotmail.com>
2026-03-13 11:07:08 +0800

    feat: add main index.js file to tool-easy project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+console.log('Tool Easy Project is running!');
+
+function easyTool() {
+    console.log('This is an easy tool for developers!');
+}
+
+module.exports = { easyTool };
